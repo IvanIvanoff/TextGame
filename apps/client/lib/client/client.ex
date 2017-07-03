62d018a375a252ee7f33c49bdbe17e120f57792c
Ivan Aleksandroff <ivan.aleksandrov@opencode.com>
2017-07-03 18:14:18 +0300

    some client docs

--- apps/client/lib/client/client.ex
+++ apps/client/lib/client/client.ex
@@ -1,36 +1,57 @@
 defmodule Client do
+  @client :tg_client
+  @server_location "127.0.0.1"
+
   def lift() do
-    Client.Application.start(nil, ["127.0.0.1"])
+    Client.Application.start(nil, [@server_location])
     Process.sleep(500)
     connect()
   end
 
+  @doc """
+    Connect to the game server
+  """
   def connect do
-    GenServer.call(:tg_client, :connect)
+    GenServer.call(@client, :connect)
   end
 
+  @doc """
+    Disconnect from the game server
+  """
   def disconnect do
-    GenServer.call(:tg_client, :disconnect)
+    GenServer.call(@clientt, :disconnect)
   end
 
+  @doc """
+    Query the current question.
+  """
   def question? do
-    GenServer.call(:tg_client, :get_question)
+    GenServer.call(@client, :get_question)
   end
 
+  @doc """
+    Query the current score ranking.
+  """
   def ranking do
-    GenServer.call(:tg_client, :ranking)
+    GenServer.call(@client, :ranking)
   end
 
+  @doc """
+    Display all players in the current game
+  """
   def list_players do
-    list = GenServer.call(:tg_client, :list_players)
+    list = GenServer.call(@client, :list_players)
 
-    IO.puts("tgs online:")
+    IO.puts("Players in the game:")
     IO.puts(IO.ANSI.green())
     list |> Enum.each(&IO.puts/1)
     IO.puts(IO.ANSI.reset())
   end
 
-  def msg(message) do
-    GenServer.cast(:tg_client, {:send_message, message})
+  @doc """
+    Send a message to all players
+  """
+  def send(message) do
+    GenServer.cast(@client, {:send_message, message})
   end
 end
